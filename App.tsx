21f638f6e67a6542d50d865ea9ab1a412d52eeb1
shiyuanyuanyuan <chen.shiyua@northeastern.edu>
2025-01-13 14:46:23 -0800

    Add TextInput component to App for user input

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 import Header from './components/Header'
+import React, { useState } from 'react'
 
 export default function App() {
   const appName = 'my app'
+  const [text, setText] = useState('')
   return (
     <View style={styles.container}>
       <Header name={appName} />
       <StatusBar style="auto" />
+      <TextInput 
+        value = {text} 
+        onChangeText={(newText) => setText(newText)}
+        placeholder='type here'/>
     </View>
   );
 }

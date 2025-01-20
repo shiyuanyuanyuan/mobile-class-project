dc97b28f3a477284bce73127373429c20e162169
shiyuanyuanyuan <chen.shiyua@northeastern.edu>
2025-01-20 14:24:05 -0800

    Add modal for input confirmation and manage visibility in Input component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import Header from './components/Header'
 import React, { useState } from 'react'
 import Input from './components/Input'
@@ -7,17 +7,23 @@ import Input from './components/Input'
 export default function App() {
   const appName = 'my app'
   const [receivetText, setReceiveText] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
   function handleInputData(data: string){
     console.log("app user type: ", data)
     setReceiveText(data)
+    setModalVisible(false)
+  }
+  function handleModal(){
+    setModalVisible(true)
   }
 
   return (
     <View style={styles.container}>
       <Header name={appName} />
       <StatusBar style="auto" />
-      <Input focus={true} inputHandler={handleInputData} />
+      <Input focus={true} inputHandler={handleInputData} modalVisibility={modalVisible} />
       <Text>{receivetText}</Text>
+      <Button title="Add a goal" onPress={handleModal}></Button>
     </View>
   );
 }
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,12 +1,23 @@
-import { View, TextInput, Text, Button} from 'react-native'
+import { View, TextInput, Text, Button, StyleSheet, Modal} from 'react-native'
 import React from 'react'
 import { useState } from 'react'
+
 interface InputProps {
   focus?: boolean;
   inputHandler: (data: string) => void;
+  modalVisibility: boolean
 }
 
-const Input = ({ focus: initialFocus = false, inputHandler }: InputProps) => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Input = ({ focus: initialFocus = false, inputHandler, modalVisibility }: InputProps) => {
   const [text, setText] = useState('')
   const [isFocused, setIsFocused] = useState(initialFocus)
   function handleConfirm() {
@@ -15,27 +26,30 @@ const Input = ({ focus: initialFocus = false, inputHandler }: InputProps) => {
   }
 
   return (
-    <View>
-      <TextInput 
-        value={text} 
-        onChangeText={(newText) => setText(newText)}
-        placeholder='type here'
-        autoFocus={initialFocus}
-        onSubmitEditing={(e) => {
-          e.target.blur()
-          setIsFocused(false)
-        }}
-        onFocus={() => setIsFocused(true)}
-      />
-      
-      {text.length > 0 && isFocused && <Text>{text.length} characters are entered</Text>}
-      {text.length >= 3 && !isFocused && <Text>Thank you</Text>}
-      {text.length < 3 && !isFocused && <Text>Please type more than 3 characters</Text>}
-      <Button title="confirm" onPress={handleConfirm}>
+    <Modal visible={modalVisibility} animationType='slide'>
+      <View style={styles.container}>
+        <TextInput 
+          value={text} 
+          onChangeText={(newText) => setText(newText)}
+          placeholder='type here'
+          autoFocus={initialFocus}
+          onSubmitEditing={(e) => {
+            e.target.blur()
+            setIsFocused(false)
+          }}
+          onFocus={() => setIsFocused(true)}
+        />
         
-      </Button>
-    </View>
+        {text.length > 0 && isFocused && <Text>{text.length} characters are entered</Text>}
+        {text.length >= 3 && !isFocused && <Text>Thank you</Text>}
+        {text.length < 3 && !isFocused && <Text>Please type more than 3 characters</Text>}
+        <Button title="confirm" onPress={handleConfirm}></Button>
+        
+      </View>
+    </Modal>
   )
 }
+
+
 
 export default Input
